f823c40c50ddc303f30c2149d75224ff194ecd1a
artik292 <artik292@gmail.com>
2018-03-20 19:26:10 +0200

    /

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $app->add(['Button', 'Send testing sms'])->on('click',  function() use($app) {
 });
 
 function isMobile() {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+    //return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
 
 if(isMobile()){
